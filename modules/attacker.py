fbd651b0eab34ebe10420678738e7f2b7bc2e70b
Nav-Prak <naveenprakaasam@gmail.com>
2025-05-04 20:26:41 -0700

    Fixed bug

--- modules/attacker.py
+++ modules/attacker.py
@@ -33,7 +33,7 @@ def call_deepseek_openrouter(
             "X-Title": "PurpleOps"
         }
     )
-    return resp.choices[0].message.content.strip()
+    return resp["choices"][0]["message"]["content"].strip()
 
 def generate_attacks(
     base_prompt: str,
